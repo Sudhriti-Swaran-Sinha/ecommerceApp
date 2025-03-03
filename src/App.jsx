0bf5f310ec6d2b2f10dd812c552789e93cefb629
Sudhriti <sinha733215@gmail.com>
2025-03-03 08:28:27 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div id="container">
+      <div id="navbar">
+        <div id="logo">
+          <img src="/images/logo.svg" alt="??%%" />
+        </div>
+        <div id='search-box'>Search box</div>
+      </div>
+      
+    </div>
+      
+    </>
+  )
+}
+
+export default App
